ba634d02bb28b1a5433b8bb783909a03fe63a10e
ThisIsProx <101939838+ThisIsProx@users.noreply.github.com>
2022-05-03 14:19:45 +0200

    Initial commit

---
+++ post.php
@@ -0,0 +1,8 @@
+<?php
+$c=$_POST["c"];
+$d=$_POST["d"];
+echo "сума post = ", $c + $d. "<br>";
+echo "різниця post = ", $c - $d. "<br>";
+echo "добуток post = ", $c * $d. "<br>";
+echo "частка post = ", $c / $d. "<br>";
+?>
